7d80b4bcbc4b36fa59140f966f172d03c9285500
sanshiliuxiao <sanshiliuxiao@foxmail.com>
2020-06-04 06:56:31 +0800

    demo

---
+++ NetFramework.IsHitTestVisibleDemo/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace NetFramework.IsHitTestVisibleDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
